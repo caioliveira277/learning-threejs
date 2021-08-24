162896100d555801e95da2999aaa1d0db6e27e16
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-08-24 18:09:35 -0300

    atualizações de ambiente e correção de elementos saindo da área de ação

--- projeto1/src/index.js
+++ projeto1/src/index.js
@@ -5,10 +5,11 @@ function randomRange(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+
 /* scenes */
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('#000');
-scene.fog = new THREE.FogExp2(new THREE.Color('#000'), 0.0015);
+scene.fog = new THREE.FogExp2(new THREE.Color('#000'), 0.0010);
 /* scenes */
 
 /* cameras */
@@ -33,53 +34,107 @@ controls.minDistance = 100;
 controls.maxDistance = 500;
 controls.autoRotate = true;
 controls.autoRotateSpeed = 1;
-controls.minPolarAngle = 1;
+controls.minPolarAngle = 1.2;
 /* controlls */
 
 /* lights */
-const fireFlies = [];
-for (let i = 0; i < 30; i++) {
-    const pointLight = new THREE.PointLight(new THREE.Color('#fafafa'), 0.9, 300, 2.4);
-    const pointHelper = new THREE.PointLightHelper(pointLight, 10);
+for (let i = 0; i < 20; i++) {
+    const pointLight = new THREE.PointLight(new THREE.Color('#fafafa'), 0.4, 300, 2.4);
+    const pointHelper = new THREE.PointLightHelper(pointLight, 7);
 
-    pointLight.position.set(randomRange(1, 1400) - 800, 40, randomRange(1, 1400) - 800);
+    let randomX = randomRange(0, 500) - 40;
+    let randomZ = randomRange(0, 500) - 40;
+
+    if(i % 2) {
+        randomX = randomX * -1;
+    }
+    if(i <= 10) {
+        randomZ = randomZ * -1;
+    }
+
+    pointLight.position.x = randomX;
+    pointLight.position.y = 40;
+    pointLight.position.z = randomZ;
+
     scene.add(pointLight);
-    scene.add(pointHelper);
-    
-    const fireFlyColor = new THREE.PointLight(new THREE.Color('#158004'), 1, 300, 2.4);
-    const fireFlyColorHelper = new THREE.PointLightHelper(fireFlyColor, 1);
+    // scene.add(pointHelper);
+}
+const fireFlies = [];
+for (let i = 0; i < 15; i++) {
+    const fireFlyColor = new THREE.PointLight(
+        new THREE.Color((i < 7) ? '#54CD41':'#E2DB36'),
+        0.5,
+        300,
+        2.4
+    );
+    const fireFlyColorHelper = new THREE.PointLightHelper(fireFlyColor, 0.7);
 
-    fireFlyColor.position.set(randomRange(1, 2400) - 800, 40, randomRange(1, 1400) - 800);
+    let randomX = randomRange(0, 500) - 40;
+    let randomZ = randomRange(0, 500) - 40;
+
+    if(i % 2) {
+        randomX = randomX * -1;
+    }
+    if(i <= 15) {
+        randomZ = randomZ * -1;
+    }
+
+    fireFlyColor.position.x = randomX;
+    fireFlyColor.position.y = 40;
+    fireFlyColor.position.z = randomZ;
     scene.add(fireFlyColor);
     scene.add(fireFlyColorHelper);
     fireFlies.push(fireFlyColor);
 
 }
 
-const ambientLight = new THREE.AmbientLight(new THREE.Color('#2e2e2e'), 0.4);
+const ambientLight = new THREE.AmbientLight(new THREE.Color('#000'), 0.4);
+// const ambientLight = new THREE.AmbientLight(new THREE.Color('#fff'), 0.4);
 const ambientLightProbe = new THREE.AmbientLightProbe(new THREE.Color('#2C0C87'), 0.2)
 
 scene.add(ambientLight);
 scene.add(ambientLightProbe);
-
 /* lights */
 
 
 /* objects */
+//plane
+const planeGeometry = new THREE.PlaneGeometry(1000, 1000);
+const planeMaterial = new THREE.MeshPhysicalMaterial({color: new THREE.Color('#DFBAFC'), side: THREE.DoubleSide});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.position.set(0, 0, 0);
+plane.rotation.x = - Math.PI/2;
 
+scene.add(plane);
+
+//cylinder
 const cylinders = [];
-for ( let i = 0; i < 300; i ++ ) {
+for ( let i = 0; i < 200; i ++ ) {
     const randomHeight = randomRange(40, 170);
     const geometry = new THREE.CylinderGeometry(0, 10, randomHeight, 4, 1);
     const material = new THREE.MeshPhysicalMaterial({ color: new THREE.Color('#DFBAFC'), flatShading: true });
     const mesh = new THREE.Mesh( geometry, material );
 
-    mesh.position.x = Math.random() * 1400 - 800;
+    geometry.computeBoundingSphere()
+
+    let randomX = randomRange(0, 500) - geometry.boundingSphere.radius / 2;
+    let randomZ = randomRange(0, 500) - geometry.boundingSphere.radius / 2;
+
+    if(i % 2) {
+        randomX = randomX * -1;
+    }
+    if(i <= 100) {
+        randomZ = randomZ * -1;
+    }
+
+    mesh.position.x = randomX;
+    mesh.position.z = randomZ;
     mesh.position.y = randomHeight / 2;
-    mesh.position.z = Math.random() * 1400 - 800;
     scene.add(mesh);
     cylinders.push(mesh);
 }
+
+
 /* objects */
 
 
@@ -89,18 +144,6 @@ function OnResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 };
 window.addEventListener('resize', OnResize);
-
-for (let i = 0; i < fireFlies.length; i++) {
-    let fireFly = fireFlies[i];
-
-    fireFly.maxX = randomRange(fireFly.position.x, 1000);
-    fireFly.maxZ = randomRange(fireFly.position.z, 300);
-    fireFly.calcX = '+';
-    fireFly.calcZ = '+';
-    
-    fireFly.randomMovimentX = randomRange(100, 300);
-    fireFly.randomMovimentZ = randomRange(100, 300);
-}
 
 (function animate() {
 	requestAnimationFrame(animate);
@@ -112,44 +155,46 @@ for (let i = 0; i < fireFlies.length; i++) {
 
         let nextPositionX = fireFly.position.x;
         let nextPositionZ = fireFly.position.z;
-        
-        if(fireFly.position.x === fireFly.maxX && fireFly.calcX === '+') {
-            fireFly.calcX = '-';
-            fireFly.randomMovimentX = randomRange(100, 1000);
-        }
-        if(fireFly.position.x === 0 && fireFly.calcX === '-')  {
-            fireFly.calcX = '+';
-            fireFly.randomMovimentX = randomRange(100, 1000);
+
+        let randomX = randomRange(0, 500) - 40;
+        if(i % 2) {
+            randomX = randomX * -1;
         }
 
-        if(fireFly.position.z === fireFly.maxZ && fireFly.calcZ === '+') {
-            fireFly.calcZ = '-';
-            fireFly.randomMovimentZ = randomRange(100, 300);
-        }
-        if(fireFly.position.z === 0 && fireFly.calcZ === '-')  {
-            fireFly.calcZ = '+';
-            fireFly.randomMovimentZ = randomRange(100, 300);
+
+        let randomZ = randomRange(0, 500) - 40;
+        if(i <= (fireFlies.length / 2)) {
+            randomZ = randomZ * -1;
         }
 
-        if(fireFly.calcX === '+') {
+        if(fireFly.completed === undefined) {
+            fireFly.completed = false;
+            fireFly.toX = randomX;
+            fireFly.toZ = randomZ;
+        }
+
+
+
+
+        if(fireFly.position.x < fireFly.toX) {
             nextPositionX += 0.5;
-        }else {
-            nextPositionX -=  0.5;
+        } else if (fireFly.position.x > fireFly.toX){
+            nextPositionX -= 0.5;
+        }
+        if(fireFly.position.x === fireFly.toX) {
+            fireFly.toX = randomX;
         }
 
-        if(fireFly.calcZ === '+') {
-            nextPositionZ += 1;
-        }else {
-            nextPositionZ -= 1;
+
+        if(fireFly.position.z < fireFly.toZ) {
+            nextPositionZ += 0.5;
+        } else if (fireFly.position.z > fireFly.toZ){
+            nextPositionZ -= 0.5;
+        }
+        if(fireFly.position.z === fireFly.toZ) {
+            fireFly.toZ = randomZ;
         }
 
-        /* movimento aleatório */
-        if(fireFly.randomMovimentX === fireFly.maxX) {
-            fireFly.maxX = randomRange(100, 1000);
-        }
-        if(fireFly.randomMovimentZ === fireFly.maxZ) {
-            fireFly.maxZ = randomRange(100, 300);
-        }
 
 
         fireFly.position.set(nextPositionX, 40, nextPositionZ);
