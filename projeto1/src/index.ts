bf2eb4035c58070393d704bc101691c2eaf557e4
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-08-25 14:00:53 -0300

    removendo importações desnecessárias

--- projeto1/src/index.ts
+++ projeto1/src/index.ts
@@ -1,8 +1,3 @@
-// import '../src/scenes';
-// import '../src/renderers';
-// import '../src/cameras';
 import '../src/listeners';
 import '../src/objects';
-import '../src/lights';
-import '../src/controls';
 import '../src/animates';
