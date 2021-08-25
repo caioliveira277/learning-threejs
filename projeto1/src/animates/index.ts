346a552fb65ce5e6d554ffa47d884f1ede70f1c0
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-08-25 13:59:34 -0300

    implementando classe de renderização das animações

--- projeto1/src/animates/index.ts
+++ projeto1/src/animates/index.ts
@@ -1,61 +1,5 @@
-// (function animate() {
-// 	requestAnimationFrame(animate);
-
-//     controls.update();
-
-//     for (let i = 0; i < fireFlies.length; i++) {
-//         let fireFly = fireFlies[i];
-
-//         let nextPositionX = fireFly.position.x;
-//         let nextPositionZ = fireFly.position.z;
-
-//         let randomX = randomRange(0, 500) - 40;
-//         if(i % 2) {
-//             randomX = randomX * -1;
-//         }
-
-
-//         let randomZ = randomRange(0, 500) - 40;
-//         if(i <= (fireFlies.length / 2)) {
-//             randomZ = randomZ * -1;
-//         }
-
-//         if(fireFly.completed === undefined) {
-//             fireFly.completed = false;
-//             fireFly.toX = randomX;
-//             fireFly.toZ = randomZ;
-//         }
-
-
-
-
-//         if(fireFly.position.x < fireFly.toX) {
-//             nextPositionX += 0.5;
-//         } else if (fireFly.position.x > fireFly.toX){
-//             nextPositionX -= 0.5;
-//         }
-//         if(fireFly.position.x === fireFly.toX) {
-//             fireFly.toX = randomX;
-//         }
-
-
-//         if(fireFly.position.z < fireFly.toZ) {
-//             nextPositionZ += 0.5;
-//         } else if (fireFly.position.z > fireFly.toZ){
-//             nextPositionZ -= 0.5;
-//         }
-//         if(fireFly.position.z === fireFly.toZ) {
-//             fireFly.toZ = randomZ;
-//         }
-
-
-
-//         fireFly.position.set(nextPositionX, 40, nextPositionZ);
-//     }
-
-	// renderer.render(scene, camera);
-// })();
-
+import Lights from '../lights';
+import OrbitControls from '../controls';
 import WebGLRenderer from '../renderers';
 import ForestScene from '../scenes';
 import PerspectiveCamera from '../cameras';
@@ -68,9 +12,13 @@ class Animates {
     private setCoreAnimate(): void {
         (function animate() {
             requestAnimationFrame(animate);
+
+            OrbitControls.update();
+
+            Lights.setMovingLightsAnimate();
+
             WebGLRenderer.render(ForestScene, PerspectiveCamera);
-        })()
-        // controls.update();
+        })();
     }
 }
 
