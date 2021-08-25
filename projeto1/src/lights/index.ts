56a48f29bcb19855caae759187e9b475dc80df6f
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-08-25 10:37:19 -0300

    finalizando implementação das luzes

--- projeto1/src/lights/index.ts
+++ projeto1/src/lights/index.ts
@@ -1,13 +1,13 @@
-import { 
+import {
     PointLight,
     PointLightHelper,
     AmbientLight,
     AmbientLightProbe,
     Color
 } from 'three';
-import { 
-    randomizeRange, 
-    randomizeAxisValues 
+import {
+    randomizeRange,
+    randomizeAxisValues
 } from '../utils';
 import ForestScene from '../scenes';
 
@@ -15,19 +15,19 @@ import ForestScene from '../scenes';
 class Lights {
     private readonly maxLightPoints = 20;
     private readonly maxMovingLights = 15;
-    public movingLightsRendered: PointLight[] = [];
+    public renderedMovingLights: PointLight[] = [];
 
     constructor() {
         this.setLightPoints();
         this.setMovingLights();
-        this.setAmbientLights();
+        this.setEnvironmentLights();
     }
 
     private setLightPoints(): void {
         for (let i = 0; i < this.maxLightPoints; i++) {
-            const lightPoint = new PointLight(new Color('#fafafa'), 0.4, 300, 2.4);
-            const lightPointHelper = new PointLightHelper(lightPoint, 7);
-        
+            const pointLight = new PointLight(new Color('#fafafa'), 0.4, 300, 2.4);
+            const lightPointLight = new PointLightHelper(pointLight, 7);
+
             const randomAxis = randomizeAxisValues({
                 x: {
                     value: randomizeRange(0, 500) - 40
@@ -38,12 +38,12 @@ class Lights {
                 }
             });
 
-            lightPoint.position.x = randomAxis.x.value;
-            lightPoint.position.y = 40;
-            lightPoint.position.z = randomAxis.z.value;
-        
-            ForestScene.add(lightPoint);
-            ForestScene.add(lightPointHelper);
+            pointLight.position.x = randomAxis.x.value;
+            pointLight.position.y = 40;
+            pointLight.position.z = randomAxis.z.value;
+
+            ForestScene.add(pointLight);
+            ForestScene.add(lightPointLight);
         }
     }
 
@@ -51,8 +51,8 @@ class Lights {
         for (let i = 0; i < this.maxMovingLights; i++) {
             const dynamicColor = this.maxMovingLights % 2 ? '#54CD41':'#E2DB36';
 
-            const lightPoint = new PointLight(new Color(dynamicColor), 0.5, 300, 2.4);
-            const lightPointHelper = new PointLightHelper(lightPoint, 0.7);
+            const pointLight = new PointLight(new Color(dynamicColor), 0.5, 300, 2.4);
+            const lightPointLight = new PointLightHelper(pointLight, 0.7);
 
             const randomAxis = randomizeAxisValues({
                 x: {
@@ -64,19 +64,19 @@ class Lights {
                 }
             });
 
-            lightPoint.position.x = randomAxis.x.value;
-            lightPoint.position.y = 40;
-            lightPoint.position.z = randomAxis.z.value;
+            pointLight.position.x = randomAxis.x.value;
+            pointLight.position.y = 40;
+            pointLight.position.z = randomAxis.z.value;
 
-            ForestScene.add(lightPoint);
-            ForestScene.add(lightPointHelper);
+            ForestScene.add(pointLight);
+            ForestScene.add(lightPointLight);
 
-            this.movingLightsRendered.push(lightPoint);
+            this.renderedMovingLights.push(pointLight);
 
         }
     }
 
-    private setAmbientLights(): void {
+    private setEnvironmentLights(): void {
         const ambientLight = new AmbientLight(new Color('#000'), 0.4);
         const ambientLightProbe = new AmbientLightProbe(new Color('#2C0C87'), 0.2);
 
@@ -85,4 +85,4 @@ class Lights {
     }
 }
 
-export default new Lights();
+export default new Lights;
