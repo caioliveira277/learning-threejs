fec76df04fc31ae58554848c3e8aa5aaf3b20c3a
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-08-24 23:01:22 -0300

    adicionando lib e configurações do typescript

--- projeto1/webpack.config.js
+++ projeto1/webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  mode: 'development'
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'public'),
+    },
 };
